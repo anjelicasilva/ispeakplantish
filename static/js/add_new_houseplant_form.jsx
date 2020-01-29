0c975189432a7f05d01bac34fbd25e15e965c921
Anjelica <anjelicacsilva@gmail.com>
2020-01-29 21:15:10 +0000

    Add dropdown menu for Latin Name

--- static/js/add_new_houseplant_form.jsx
+++ static/js/add_new_houseplant_form.jsx
@@ -4,37 +4,51 @@ class Form extends React.Component {
     constructor(props) {
         super(props);
         this.state =  {
-            selectedOption: "low"
+            selectedLight: "low",
+            selectedLatinName: "latin test"
         };
     }
     
-    handleOptionChange = changeEvent => {
+    handleLightChange = changeEvent => {
         this.setState({
-            selectedOption: changeEvent.target.value
+            selectedLight: changeEvent.target.value
+        });
+    };
+
+    handleLatinChange = changeEvent => {
+        this.setState({
+            selectedLatinName: changeEvent.target.value
         });
     };
     
     handleFormSubmit = formSubmitEvent => {
         formSubmitEvent.preventDefault();
 
-        console.log("You have submitted: ", this.state.selectedOption);
+        console.log("You have submitted: ", this.state.selectedLight);
+        console.log("You have submited: ", this.state.selectedLatinName);
     };
     
     render() {
         return (// 
 
-        <form onSubmit={this.handleFormSubmit}>
+        <form onSubmit={this.handleFormSubmit}>  
             <label>
                 Latin Name:
-                <input type="text" name="latinName"></input>
-            </label>
+                <select
+                    value={this.selectedLatinName}
+                    onChange={this.handleLatinChange}
+                    className="latin-name"
+                    id="latinName"
+                >
+                    <option value="noNewHouseplant">Select latin name</option>
+                    <option value="adiantumFragrans">Adiantum fragrans</option>
+                    <option value="aechmeaBlueRain">Aechmea 'Blue Rain'</option>
+                    <option value="aeschynanthusJaphrolepis">Aeschynanthus japhrolepis</option>
+                </select>
+            </label>   
             <label>
                 Common Name:
                 <input type="text" name="commonName"></input>
-            </label>
-            <label>
-                Notes:
-                <input type="text" name="notes"></input>
             </label>
             
             <label>
@@ -47,8 +61,8 @@ class Form extends React.Component {
                         type="radio" 
                         name="lightRequirement"
                         value="low"
-                        checked={this.state.selectedOption==="low"}
-                        onChange={this.handleOptionChange}
+                        checked={this.state.selectedLight==="low"}
+                        onChange={this.handleLightChange}
                         className="light-requirement-input"
                     />
                     Low
@@ -58,8 +72,8 @@ class Form extends React.Component {
                         type="radio" 
                         name="lightRequirement"
                         value="medium"
-                        checked={this.state.selectedOption==="medium"}
-                        onChange={this.handleOptionChange}
+                        checked={this.state.selectedLight==="medium"}
+                        onChange={this.handleLightChange}
                         className="light-requirement-input"
                     />
                     Medium
@@ -69,15 +83,21 @@ class Form extends React.Component {
                         type="radio" 
                         name="lightRequirement"
                         value="high"
-                        checked={this.state.selectedOption==="high"}
-                        onChange={this.handleOptionChange}
+                        checked={this.state.selectedLight==="high"}
+                        onChange={this.handleLightChange}
                         className="light-requirement-input"
                     />
                     High
                 </label>
             </div>
-            
-                <input type="submit" value="Add Houseplant"></input>
+                <button
+                    type="submit"
+                    className="btn btn-primary"
+                    disabled={this.state.selectedLatinName === "noNewHouseplant"}
+                >
+                    Add Houseplant
+                </button>
+                {/* <input type="submit" value="Add Houseplant"></input> */}
             </form>);
     }
 }
