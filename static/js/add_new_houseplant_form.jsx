7175119a905cd3234e03e17bff3f39d29c9c3c15
Anjelica <anjelicacsilva@gmail.com>
2020-02-08 21:14:28 +0000

    Create dropdown menu of common houseplant names and automatically generate latin name

--- static/js/add_new_houseplant_form.jsx
+++ static/js/add_new_houseplant_form.jsx
@@ -5,25 +5,33 @@ class Form extends React.Component {
        super(props);
        this.state =  {
            selectedLight: "low",
-           selectedLatinName: "noNewHouseplant",
-           selectedCommonName: "noNewHouseplant",
+           selectedCommonName: null,
+           selectedLatinName: null,
+
            listOfCommonHouseplants: [],
        };
+       this.generateLatinName = this.generateLatinName.bind(this);
    }
- 
+
    async componentDidMount() {
+
        const response = await fetch(`/api/common_houseplants`);
        const myJson = await response.json();
-
-
-    //    This is how to access common name!
-    //    console.log('LLOOOOOOKKKK')
-    //    console.log('myJson entries:', Object.entries(myJson)["Adiantum fragrans"][1]['common_name']);
  
-       for (const plant of Object.keys(myJson)) {
+       for (const plantObject of Object.entries(myJson)) {
+   
+            let plant = {
+               commonHouseplantId: plantObject[1]['common_houseplant_id'],
+               commonHouseplantPhotoUrl: plantObject[1]['common_houseplant_photo_url'],
+               commonName: plantObject[1]['common_name'],
+               latinName: plantObject[1]['latin_name'],
+               generalDescription: plantObject[1]['general_description'],
+               recommendedLightRequirements: plantObject[1]['light_requirements'],
+           }
+
            this.state.listOfCommonHouseplants.push(plant);
        };
- 
+
        this.setState({
            listOfCommonHouseplants: this.state.listOfCommonHouseplants
        });
@@ -37,50 +45,69 @@ class Form extends React.Component {
    };
  
  
-   handleLatinNameChange = changeEvent => {
-       this.setState({
-           selectedLatinName: changeEvent.target.value
-       });
-   };
-
-
    handleCommonNameChange = changeEvent => {
-    this.setState({
-        selectedCommonName: changeEvent.target.value
-    });
-};
+       const latinNameIndex = this.generateLatinName(changeEvent.target.value)
+       this.setState({
+            selectedCommonName: changeEvent.target.value,
+            selectedLatinName: this.state.listOfCommonHouseplants[latinNameIndex]['latinName']
+       });
+       
+   };
   
  
    handleFormSubmit = formSubmitEvent => {
        formSubmitEvent.preventDefault();
- 
-       console.log("You have submitted: ", this.state.selectedLight);
-       console.log("You have submited: ", this.state.selectedLatinName);
-       console.log("You have submited: ", this.state.selectedCommonName);
+        // pull out all the relevant data from state
+        // make sure it looks gucci 
+        // then make a post request with that data from state to your server
+
+        // let body = { name: this.state.name,
+        //     light: this.state.selectedLight }
+
+        // const response = await fetch(`/api/new_plant`, { 
+        //     method: 'POST', 
+        //     body: JSON.stringify(body), 
+        //     headers: {'Content-Type': 'application/json'}
+        // });
+
+        console.log("You have submited the common name: ", this.state.selectedCommonName);
+        console.log("You have submited the latin name: ", this.state.selectedLatinName);
+        console.log("You have submitted the general light requirement: ", this.state.selectedLight);
    };
  
   
    renderPlantOptions() {
-       const plantOptions = [<option value="noNewHouseplant">Select latin name</option>];
+       const plantOptions = [<option value="noNewHouseplant">Select Common Name</option>];
  
-       for (const plant of this.state.listOfCommonHouseplants) {
-           plantOptions.push(<option value={plant}>{plant}</option>)
+       for (const plantObject of this.state.listOfCommonHouseplants) {
+           plantOptions.push(<option value={plantObject['commonName']} key={plantObject['commonHouseplantId']}>{plantObject['commonName']}</option>)
        };
 
        return plantOptions
    }
+
+   generateLatinName(selectedCommonName) {
+        for (const plantObject of this.state.listOfCommonHouseplants) {
+            if (plantObject['commonName'] === selectedCommonName) {
+                let latinNameIndex = plantObject["commonHouseplantId"] - 1
+                return latinNameIndex
+            };
+        };
+        return -1
+   }
  
+
    render() {
        return (
        <form onSubmit={this.handleFormSubmit}> 
            <div>
            <label>
-               Latin Name:
+               Common Name:
                <select
-                   value={this.selectedLatinName}
-                   onChange={this.handleLatinNameChange}
-                   className="latin-name"
-                   id="latinName"
+                   value={this.selectedCommonName}
+                   onChange={this.handleCommonNameChange}
+                   className="common-name"
+                   id="CommonName"
                >
                    { this.renderPlantOptions() }
                </select>
@@ -88,15 +115,15 @@ class Form extends React.Component {
            </div>  
            <div>
            <label>
-               Common Name:
-               <input 
-                    id="commonName" 
-                    type="text" 
-                    name="commonName" 
-                    value={this.selectedCommonName} 
-                    onChange={this.handleCommonNameChange}
-                    placeholder="Enter Common Name">    
-                </input>
+               Latin Name:
+                <p
+                    id="LatinName" 
+                    name="LatinName" 
+                    value={this.selectedLatinName} 
+                    placeholder="Enter Latin Name">
+                    
+                    { this.state.selectedLatinName }
+                </p>
            </label>
            </div>
            <div className="light-requirement">
@@ -141,7 +168,7 @@ class Form extends React.Component {
                <button
                    type="submit"
                    className="btn btn-primary"
-                   disabled={this.state.selectedLatinName === "noNewHouseplant"}
+                   disabled={this.state.selectedCommonName === null}
                >
                    Add Houseplant
                </button>
