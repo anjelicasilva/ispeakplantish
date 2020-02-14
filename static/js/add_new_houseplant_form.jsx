8e228fc79c9613fd14bc2f4720a52b968395f190
Anjelica <anjelicacsilva@gmail.com>
2020-02-14 03:32:02 +0000

    Add brief description of the purpose of this web app

--- static/js/add_new_houseplant_form.jsx
+++ static/js/add_new_houseplant_form.jsx
@@ -202,10 +202,35 @@ class HomePage extends React.Component {
 } 
 
 
+// class AboutPage extends React.Component {
+//     render() {
+//     return (
+//       <div> About Us </div>
+//     );
+//     }
+// }
+
+
 class AboutPage extends React.Component {
     render() {
     return (
-      <div> About Us </div>
+      <div>
+        <h3>
+          ISpeakPlantish
+        </h3>
+        <p>
+          Looking to transform your brown thumb green?
+        </p>
+        <p>
+          A green thumb is not about luck, but about being observant 
+          and knowing how Mother Nature takes care of her house plants!
+        </p>
+        <p>
+          ISpeakPlantish is an indoor gardening notebook-style web application 
+          that offers tracker journal logs to grow, nurture, and care for your 
+          small-space greenery.
+        </p>
+      </div>
     );
     }
 }
@@ -492,7 +517,7 @@ class App extends React.Component {
                     this.setState({currentPage: 0})}>Homepage
                     </button> 
                     <button onClick={() => 
-                    this.setState({currentPage: 1})}>ISpeakPlantish
+                    this.setState({currentPage: 1})}>About
                     </button> 
                     <button onClick={() => 
                     this.setState({currentPage: 2})}>Add a plant
