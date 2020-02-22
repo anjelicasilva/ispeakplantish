e309b2688b7443fd8037f71d0944d499998915e6
Anjelica <anjelicacsilva@gmail.com>
2020-02-22 23:57:01 +0000

    Implement an autocomplete search bar for common houseplants to add to users plant collection

---
+++ static/js/autocompleteText.jsx
@@ -0,0 +1,52 @@
+class AutoCompleteText extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            suggestions: [],
+            currentResult: '',
+        };
+
+        this.renderSuggestions = this.renderSuggestions.bind(this);
+        this.suggestionSelected = this.suggestionSelected.bind(this);
+    }
+
+    onTextChanged = (event) => {
+        const { items } = this.props;
+        const value = event.target.value;
+        let suggestions = [];
+        if (value.length > 0) {
+            const regex = new RegExp(`^${value}`, 'i');
+            suggestions = items.sort().filter(v => regex.test(v));
+        }
+        this.setState(() => ({ suggestions, currentResult: value }));
+    }
+
+    suggestionSelected (value) {
+        this.setState({
+            currentResult: value,
+            suggestions: [],
+        }, () =>  console.log('current state', this.state) );
+        this.props.handleCommonNameSelection(value);
+    }
+
+    renderSuggestions () {
+        const { suggestions } = this.state;
+        if (suggestions.length === 0) {
+            return null;
+        }
+        return (
+            <ul>
+            {suggestions.map((item) => <li key={item} onClick={() => this.suggestionSelected(item)}>{item}</li>)}
+            </ul>
+        );
+    }
+
+    render() {
+        return(
+            <div className="AutoCompleteText">
+                <input value={this.state.currentResult} onChange={this.onTextChanged} type="text" />
+                {this.renderSuggestions()}
+            </div>
+        )
+    }
+}
