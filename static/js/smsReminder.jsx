09c8a1f834f933f26f7dce3e0c7393b25d0f7370
Anjelica <anjelicacsilva@gmail.com>
2020-02-24 19:45:39 +0000

    Allow users to send themselves a SMS reminder to either water or fertilize their plant

--- static/js/smsReminder.jsx
+++ static/js/smsReminder.jsx
@@ -1,10 +1,73 @@
 class SmsReminder extends React.Component {
     constructor(props) {
       super(props);
-  }
+      this.state = {
+          selectedSMSReminder: null,
+      };
+    }
+
+    handleEntryInput = changeEvent => {
+        this.setState({
+          selectedSMSReminder: changeEvent.target.value
+        });
+      }
+
+
+    sendSMS = formSubmitEvent => {
+        formSubmitEvent.preventDefault();
+ 
+         let smsData = { 
+             selectedSMSReminder: this.state.selectedSMSReminder,
+         }
+ 
+         $.post('/send_sms', smsData, (response) => console.log('Send SMS reminder:', response))
+    };
+
+
     render() {
-    return (
-      <div>Send an sms reminder</div>
-    );
+        return (
+            <div>
+                <div>Text yourself a reminder so you don't forget!</div>
+                <br></br>
+                <form onSubmit={this.sendSMS}> 
+                <div className="fertilizer-update">
+                    <label>
+                    What type of reminder would you like to receive?
+                    </label>
+                    <br></br>
+                    <label>
+                        <input 
+                            type="radio" 
+                            name="sendWaterSMS"
+                            value="sendWaterSMS"
+                            checked={this.state.selectedSMSReminder==="sendWaterSMS"}
+                            onChange={this.handleEntryInput}
+                            className="sendWaterSMS"
+                        />
+                        Water Reminder
+                    </label>
+                    <label>
+                        <input 
+                            type="radio" 
+                            name="sendFertilizerSMS"
+                            value="sendFertilizerSMS"
+                            checked={this.state.selectedSMSReminder==="sendFertilizerSMS"}
+                            onChange={this.handleEntryInput}
+                            className="sendFertilizerSMS"
+                        />
+                        Fertilizer Reminder
+                    </label>
+                    </div>
+                    <div>     
+                    <button
+                        type="submit"
+                        className="btn btn-primary"
+                        disabled={this.selectedSMSReminder === null}>
+                        Send SMS
+                    </button>
+                    </div>
+                </form>
+            </div>
+        );
     }
   } 
