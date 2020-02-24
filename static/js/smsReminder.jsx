cccbe16c9b05b6173ca13e282248cad497a4eb8a
Anjelica <anjelicacsilva@gmail.com>
2020-02-24 06:11:20 +0000

    Begin component to send users sms reminders

---
+++ static/js/smsReminder.jsx
@@ -0,0 +1,10 @@
+class SmsReminder extends React.Component {
+    constructor(props) {
+      super(props);
+  }
+    render() {
+    return (
+      <div>Send an sms reminder</div>
+    );
+    }
+  } 
