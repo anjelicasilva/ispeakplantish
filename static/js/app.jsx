bc2da31916a8ad5e2f3022862eb1d40798efb44d
Anjelica <anjelicacsilva@gmail.com>
2020-02-27 01:42:00 +0000

    Implement toast notifications

--- static/js/app.jsx
+++ static/js/app.jsx
@@ -3,10 +3,32 @@ class App extends React.Component {
         super(props);
 
         this.state = { 
+            notificationText: null,
             currentPage: 0, 
-            pages: [<HomePage />, <AboutPage />, <Form />, <PlantCollection />,] 
+            pages: [<HomePage />, <AboutPage />, <Form notify={this.notify}/>, <PlantCollection notify={this.notify}/>, ] 
         }; 
     }
+
+
+    notify = (notificationText) => {
+        this.setState ({
+            notificationText: notificationText
+        });
+        Toastify({
+            text: this.state.notificationText,
+            duration: 3000,
+            // destination: "<PlantCollection/>",
+            newWindow: true,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: 'right', // `left`, `center` or `right`
+            backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            // onClick: function(){} // Callback after click
+          }).showToast();
+    }
+
+
     render() {
         return (
             <div>
@@ -23,6 +45,12 @@ class App extends React.Component {
                     <button onClick={() => 
                     this.setState({currentPage: 3})}>View your plant collection
                     </button> 
+                    {/* <button onClick={() => 
+                    this.setState({currentPage: 4})}>React Toastify
+                    </button>  */}
+                    {/* <button onClick={() => 
+                    this.setState({currentPage: 5})}>Google Maps
+                    </button>  */}
                     {/* <button onClick={() => 
                     this.setState({currentPage: 4})}>Send an sms reminder
                     </button>  */}
