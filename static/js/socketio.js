27489d1e26760adf7761995cf01b908da073a032
Anjelica <anjelicacsilva@gmail.com>
2020-03-16 22:36:42 +0000

    Change socketio connection

--- static/js/socketio.js
+++ static/js/socketio.js
@@ -3,7 +3,8 @@
 // and subframes to finish loading.
 
 document.addEventListener('DOMContentLoaded', () => {
-    const socket = io.connect(('https://' + document.domain + ':' + location.port), {transports: ['websocket']});
+    const socket = io.connect(('https://ispeakplantish.com/chat'), {transports: ['websocket']});
+    // const socket = io.connect(('https://' + document.domain + ':' + location.port), {transports: ['websocket']});
 
 
     // Retrieve username
