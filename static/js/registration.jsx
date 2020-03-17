d8f4eda2b91d7ad1a4f6f31219d705a331307234
Anjelica <anjelicacsilva@gmail.com>
2020-03-17 22:19:32 +0000

    Fix spacing and indentation for the signup and registration

--- static/js/registration.jsx
+++ static/js/registration.jsx
@@ -14,7 +14,6 @@ class Register extends React.Component {
 
     handleRegistrationFormSubmit = registrationFormSubmitEvent => {
         registrationFormSubmitEvent.preventDefault();
-        
          let newUser = { 
              addFirstName: this.state.fname,
              addLastName: this.state.lname,
@@ -24,7 +23,7 @@ class Register extends React.Component {
              addReEnter: this.state.reenter,
          }
          this.props.registerUser(newUser);
-        };
+    };
 
     
     handleRegistrationInput = changeEvent => {
@@ -40,110 +39,49 @@ class Register extends React.Component {
 
     render() {
         return (
-//             <div>
-//             <form> 
-//                 <div>    
-//                 <h3>Sign Up </h3>
-//                     Please fill out the information below
-//                 <br></br>
-//                     First name:
-//                     <br></br>  
-//                     <input id="fname" type="text" name="fname" placeholder ="First" onChange={this.handleRegistrationInput}></input>
-//                 <br></br>
-//                     Last name:  
-//                     <br></br>
-//                     <input id="lname" type="text" name="lname" placeholder ="Last" onChange={this.handleRegistrationInput}></input>
-//                 <br></br>
-//                     Cellphone number: 
-//                     <br></br> 
-//                     <input id="phone" type="text" name="phone" placeholder ="Phone" onChange={this.handleRegistrationInput}></input>
-//                 <br></br>
-//                     Email address:
-//                     <br></br>  
-//                     <input id="email" type="text" name="email" placeholder ="email@domain.com" onChange={this.handleRegistrationInput}></input>
-//                 <br></br>
-//                     Password:
-//                     <br></br>  
-//                     <input id="password" type="password" name="password" placeholder ="Should be at least 7 characters" onChange={this.handleRegistrationInput}></input>
-//                 <br></br>
-//                     Re-enter Password:
-//                     <br></br>  
-//                     <input id="reenter" type="password" name='reenter' placeholder ="Should be at least 7 characters" onChange={this.handleRegistrationInput}></input> 
-//                 <br></br>
-//                 <br></br>
-//                     Register Now! 
-//                 <br></br>
-//                 </div>
-//                 <div>    
-//                     <button
-//                         onClick={this.handleRegistrationFormSubmit}
-//                     >
-//                         Sign Up
-//                     </button>
-//                 </div>    
-//             </form>
-//             </div>
-//         );
-//     }
-// } 
-
-
-
-
-
-<div id="signup-bg">
-<div className="modal-dialog text-center">
-    <div className="col-sm-8 main-section">
-        <div className="modal-content">
-
-            <div className="col-12 signup-user-img">
-                <img src="static/img/plantgif.gif"></img>
-            </div>
-
-            <form className="col-12">
-                <div className="signup-form-group">
-                    <label>
-                        <input id="fname" type="text" name="fname" className="form-control" placeholder ="First Name" onChange={this.handleRegistrationInput}></input>
-                    </label>
-                    <br></br>
-                    <label>
-                    <input id="lname" type="text" name="lname" className="form-control" placeholder ="Last Name" onChange={this.handleRegistrationInput}></input>
-                    </label>
-                    <br></br>
-
-                    <label>
-                    <input id="phone" type="text" name="phone" className="form-control" placeholder ="Phone Number" onChange={this.handleRegistrationInput}></input>
-                    </label>
-                    <br></br>
-
-                    <label>
-                    <input id="email" type="text" name="email" className="form-control" placeholder ="Email@domain.com" onChange={this.handleRegistrationInput}></input>
-                    </label>
-                    <br></br>
-
-                    <label>
-                    <input id="password" type="password" name="password" className="form-control" placeholder ="Password" minLength={5} onChange={this.handleRegistrationInput} required></input>
-                    </label>
-                    <br></br>
-
-                    <label>
-                    <input id="reenter" type="password" name='reenter' className="form-control" placeholder ="Re-enter Password" onChange={this.handleRegistrationInput}></input> 
-                    </label>
-                    <br></br>
-
-
-
-                <button className="signup-btn" onClick={this.handleRegistrationFormSubmit}> <i className="fas fa-sign-in-alt"></i>Sign Up </button>
+            <div id="signup-bg">
+                <div className="modal-dialog text-center">
+                    <div className="col-sm-8 main-section">
+                        <div className="modal-content">
+                            <div className="col-12 signup-user-img">
+                                <img src="static/img/plantgif.gif"></img>
+                            </div>
+                            <form className="col-12">
+                                <div className="signup-form-group">
+                                    <label>
+                                        <input id="fname" type="text" name="fname" className="form-control" placeholder ="First Name" onChange={this.handleRegistrationInput}></input>
+                                    </label>
+                                    <br></br>
+                                    <label>
+                                    <input id="lname" type="text" name="lname" className="form-control" placeholder ="Last Name" onChange={this.handleRegistrationInput}></input>
+                                    </label>
+                                    <br></br>
+                                    <label>
+                                    <input id="phone" type="text" name="phone" className="form-control" placeholder ="Phone Number" onChange={this.handleRegistrationInput}></input>
+                                    </label>
+                                    <br></br>
+                                    <label>
+                                    <input id="email" type="text" name="email" className="form-control" placeholder ="Email@domain.com" onChange={this.handleRegistrationInput}></input>
+                                    </label>
+                                    <br></br>
+                                    <label>
+                                    <input id="password" type="password" name="password" className="form-control" placeholder ="Password" minLength={5} onChange={this.handleRegistrationInput} required></input>
+                                    </label>
+                                    <br></br>
+                                    <label>
+                                    <input id="reenter" type="password" name='reenter' className="form-control" placeholder ="Re-enter Password" onChange={this.handleRegistrationInput}></input> 
+                                    </label>
+                                    <br></br>
+                                    <button className="signup-btn" onClick={this.handleRegistrationFormSubmit}> <i className="fas fa-sign-in-alt"></i>Sign Up </button>
+                                </div>
+                            </form>
+                            <div className="col-12 redirect-login">
+                                <a onClick={this.redirectLogin}>Already have an account?</a>
+                            </div>
+                        </div> 
+                    </div>
                 </div>
-            </form>
-
-            <div className="col-12 redirect-login">
-                <a onClick={this.redirectLogin}>Already have an account?</a>
             </div>
-        </div> 
-    </div>
-</div>
-</div>
-          );
-          }
-        }
+        );
+    }
+}
